490b3aa7232cee7defc154b603ee8bd558c8f3ff
CathalHarte <cathal.harte@rapttouch.com>
2020-04-25 14:37:59 +0200

    Adding children in a manual manner - must be disallowed next Determining if a root node by if it has a parent.

--- shared_ptr_tree/shared_ptr_tree.h
+++ shared_ptr_tree/shared_ptr_tree.h
@@ -12,6 +12,13 @@
 * Includes
 ******************************************************************************/
 
+#include <string>
+#include <memory>
+#include <vector>
+
+namespace tree
+{
+
 /*! @defgroup shared_ptr_tree Shared_ptr_tree.
  *
  * @addtogroup shared_ptr_tree
@@ -30,12 +37,29 @@
 * Class prototypes
 *******************************************************************************/
 
+class WordBranch
+{
+public:
+
+    WordBranch() : name("") {}
+    WordBranch(std::string name) : name(name) {}
+    std::string name;
+
+    // we cannot expose either the parent of vector of children
+    // else we could add children in an incorrect manner
+    std::weak_ptr<WordBranch> parent;
+    std::vector<std::shared_ptr<WordBranch>> children;
+
+    bool isRoot();
+};
+
 /*******************************************************************************
 * Function prototypes
 *******************************************************************************/
 
-
 /*! @}
  */
 
-#endif  // _SHARED_PTR_TREE_H
+}
+
+#endif // _SHARED_PTR_TREE_H
--- shared_ptr_tree/shared_ptr_tree_test/shared_ptr_tree_test.cpp
+++ shared_ptr_tree/shared_ptr_tree_test/shared_ptr_tree_test.cpp
@@ -16,6 +16,7 @@
 
 #include <shared_ptr_tree.h>
 
+using namespace tree;
 namespace
 {
 
@@ -51,6 +52,7 @@ TEST(stdlib_test, shared_out_of_scope)
     {
         std::shared_ptr<int> shared = std::make_shared<int>();
         *shared = 14;
+        // raw_ptr = *shared;
         raw_ptr = shared.get();
         top_level_weak = shared;
     }
@@ -60,6 +62,39 @@ TEST(stdlib_test, shared_out_of_scope)
     auto shared_handle = top_level_weak.lock();
     ASSERT_FALSE(shared_handle);
     ASSERT_DEATH(GTEST_COUT << *shared_handle << std::endl;, "");
+}
+
+TEST(stdlib_test, shared_string_ptr)
+{
+    std::string foo = "foo";
+    foo = "bar bar";
+
+    std::shared_ptr<char[]> shared;
+    std::shared_ptr<std::string> shared_2;
+
+    // shared_2 = "dge";
+    ASSERT_EQ(foo, "bar bar");
+    ASSERT_DEATH(shared[0] = 'a', "");
+    ASSERT_DEATH(*shared_2 = "baz", "");
+
+    std::shared_ptr<std::string> shared_3 = std::make_shared<std::string>("baz");
+
+    ASSERT_EQ(*shared_3, "baz");
+
+    *shared_3 = "qux qux";
+    ASSERT_EQ(*shared_3, "qux qux");
+
+    std::string string_2;
+    string_2 = "qux"; // this works, but not the equivalent on the shared pointer
+    // maybe that's a false equivalent
+
+    std::shared_ptr<std::string> shared_4 = std::make_shared<std::string>();
+    *shared_4 = "what's after qux?";
+    // ah, I see
+
+    // so to revise that
+    shared_2 = std::make_shared<std::string>("foo");
+    ASSERT_EQ(*shared_2, "foo");
 }
 
 TEST(stdlib_test, unique_ptr_1)
@@ -72,4 +107,72 @@ TEST(stdlib_test, unique_ptr_1)
     ASSERT_EQ(*raw_ptr, 165);
 }
 
+TEST(word_branch, declaration_bare)
+{
+    WordBranch foo;
+    ASSERT_EQ(foo.name, "");
+}
+
+TEST(word_branch, declaration_string)
+{
+    WordBranch foo("foo");
+    ASSERT_EQ(foo.name, "foo");
+}
+
+TEST(word_branch, ptr_declarations)
+{
+    std::shared_ptr<WordBranch> foo = std::make_shared<WordBranch>();
+    std::shared_ptr<WordBranch> bar = std::make_shared<WordBranch>("bar");
+    std::shared_ptr<WordBranch> baz;
+
+    ASSERT_EQ(baz.use_count(),0);
+
+    baz = bar;
+
+    ASSERT_EQ(baz.use_count(), 2);
+    ASSERT_EQ(baz->name, "bar");
+
+    bar.reset();
+    ASSERT_EQ(baz.use_count(), 1);
+    ASSERT_EQ(baz->name, "bar");
+}
+
+TEST(word_branch, out_of_scope)
+{
+    std::weak_ptr<WordBranch> on_a_branch_1, on_a_branch_2;
+    {
+        std::shared_ptr<WordBranch> trunk = std::make_shared<WordBranch>();
+        trunk->name = "I am the trunk";
+
+        {
+
+            std::shared_ptr<WordBranch> branch_1 = std::make_shared<WordBranch>();
+            branch_1->name = "I am the first branch";
+            branch_1->parent = trunk;
+            trunk->children.push_back(branch_1);
+
+            on_a_branch_1 = branch_1;
+
+            std::shared_ptr<WordBranch> branch_2 = std::make_shared<WordBranch>();
+
+            on_a_branch_2 = branch_2;
+        }
+
+        {
+            std::shared_ptr<WordBranch> temp = on_a_branch_1.lock();
+            ASSERT_EQ(temp->name, "I am the first branch");
+            ASSERT_FALSE(on_a_branch_2.lock());
+        }
+    }
+    ASSERT_FALSE(on_a_branch_1.lock());
+}
+
+
+
+// A WordBranch is a root if it has no parent, i.e. it has just been created, or if its own parent has been reset
+TEST(word_branch, is_root)
+{
+    std::shared_ptr<WordBranch> root = std::make_shared<WordBranch>("I'm the root");
+    ASSERT_TRUE(root->isRoot());
+}
 } // namespace
